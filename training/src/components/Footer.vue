457f8f8235445afe0d10b94d8138a4a485ad4d6f
Maxime Belgaïd <maxime.belgaid@epfedu.fr>
2019-09-26 20:25:21 +0200

    Footer created

---
+++ training/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer id="footer">
+        <h3 class="mx-auto">Formation Simplicité®</h3>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'Footer',
+        props: {
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    footer {
+        background-color: #514FBF;
+    }
+    h3 {
+        color: white;
+        width: 300px;
+        font-size: 1em;
+    }
+</style>
