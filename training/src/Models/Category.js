a57f2ba86e5e1162cf5e3a928bc12b7b58a7b63c
maximebelgaid <max.belgaid@gmail.com>
2019-12-12 15:41:32 +0100

    created Category and ContentItem (lesson) classes

---
+++ training/src/Models/Category.js
@@ -0,0 +1,13 @@
+export class Category {
+    constructor(row_id, trnCatOrder, trnCatTitle, trnCatDescription, trnCatPath, trnCatId, trnCatId_trnCatTitle, trnCatId_trnCatPath){
+        this.row_id = row_id;
+        this.order = trnCatOrder;
+        this.title = trnCatTitle;
+        this.description = trnCatTitle;
+        this.description = trnCatDescription;
+        this.path = trnCatPath;
+        this.parentId = trnCatId_trnCatId;
+        this.parentTitle = trnCatId_trnCatTitle;
+        this.parentPath = trnCatId_trnCatPath;
+    }
+}
---
+++ training/src/Models/ContentItem.js
@@ -0,0 +1,18 @@
+export class ContentItem {
+    constructor(row_id, trnLsnOrder, trnLsnTitle, trnLsnDescription, trnLsnVideoUrl, trnLsnContent, trnLsnPath,
+                trnLsnCatId, trnLsnCatId_trnCatTitle, trnLsnCatId_trnCatPath){
+        this.row_id = row_id;
+        this.order = trnLsnOrder;
+        this.title = trnLsnTitle;
+        this.description = trnLsnDescription;
+        this.videoUrl = trnLsnVideoUrl;
+        this.videoUrl = trnLsnVideoUrl;
+        this.content = trnLsnContent;
+        this.path = trnLsnPath;
+        this.categoryId = trnLsnCatId;
+        this.categoryTitle = trnLsnCatId_trnCatTitle;
+        this.categoryPath = trnLsnCatId_trnCatPath;
+    }
+
+
+}
