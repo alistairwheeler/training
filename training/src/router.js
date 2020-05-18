bc0497f33274c387f6f69d717d8606f88437d09f
maximebelgaid <max.belgaid@gmail.com>
2020-05-18 16:35:53 +0200

    renamed meaningfully components of Sandbox directory

--- training/src/router.js
+++ training/src/router.js
@@ -1,16 +1,16 @@
 import VueRouter from 'vue-router'
-import LessonItem from "./components/Pages/Lesson"
-import HomePage from "./components/Pages/Home"
+import Home from "./components/Pages/Home"
+import Lesson from "./components/Pages/Lesson"
 import PageNotFound from "./components/Pages/PageNotFound"
-import SandBoxRedirect from "./components/Pages/SandBoxRedirect"
-import SandBoxForm from "./components/Pages/SandBoxForm"
+import SandBoxDeployment from "./components/Pages/SandBox/Deployment"
+import SandBoxDemand from "./components/Pages/SandBox/Demand"
 
 //1. Routes definition
 const routes = [
-    { path: '/', component: HomePage },
-    { path: '/lessonItem/*', component: LessonItem },
-    { path: '/sandbox/:demandId', component: SandBoxRedirect },
-    { path: '/sandbox-demand', component: SandBoxForm },
+    { path: '/', component: Home },
+    { path: '/lessonItem/*', component: Lesson },
+    { path: '/sandbox/:demandId', component: SandBoxDeployment },
+    { path: '/sandbox-demand', component: SandBoxDemand },
     //This route is last in the list because it can override the other ones (because it matches all routes)
     { path: '/*', component: PageNotFound }
 ];
