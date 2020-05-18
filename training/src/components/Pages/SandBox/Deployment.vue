01e00c8dc06248a214cd50b7aeae246cd003d9ed
maximebelgaid <max.belgaid@gmail.com>
2020-05-18 16:44:14 +0200

    reformatted code in sandbox directory

--- training/src/components/Pages/SandBox/Deployment.vue
+++ training/src/components/Pages/SandBox/Deployment.vue
@@ -2,44 +2,50 @@
   <div class="wrapper">
     <div class="sandbox-redirect">
       <h2 class="sandbox-redirect__title">On se met au boulot, votre sandbox arrive bientôt !</h2>
-      <img class="sandbox-image" src="https://megastuces.com/wp-content/uploads/2017/02/sandbox.png" alt="sandbox image">
+      <img class="sandbox-image" src="https://megastuces.com/wp-content/uploads/2017/02/sandbox.png"
+           alt="sandbox image">
       <h2 class="sandbox-redirect__title">Quelques infos sur votre sandbox ...</h2>
       <p>Pour démarrer au mieux avec votre Simplicité, voici les liens vers quelques ressources utiles :</p>
       <ul class="sandbox-redirect__list">
         <li>
-          <p>Notre site de formation (gratuit) : <a target="_blank" href="https://docs2.dev.simplicite.io">https://docs2.dev.simplicite.io</a></p>
+          <p>Notre site de formation (gratuit) : <a target="_blank" href="https://docs2.dev.simplicite.io">https://docs2.dev.simplicite.io</a>
+          </p>
           <p>Vous y trouverez toutes les ressources pour apprendre à vous servir de Simplicité. Pour votre
             première prise en main, nous vous conseillons de suivre la formation, pour vous apprendre les bases
             du développement sur notre Plateforme.</p>
         </li>
         <li>
-          <p>Notre forum communautaire : <a target="_blank" href="http://community.simplicite.io/">http://community.simplicite.io/</a></p>
-          <p>Nos utilisateurs posent leurs questions et s'entraident sur ce forum. Vous y trouverez également des tips & tricks utiles.</p>
+          <p>Notre forum communautaire : <a target="_blank" href="http://community.simplicite.io/">http://community.simplicite.io/</a>
+          </p>
+          <p>Nos utilisateurs posent leurs questions et s'entraident sur ce forum. Vous y trouverez également des tips &
+            tricks utiles.</p>
         </li>
         <li>
-          <p>Notre documentation : <a target="_blank" href="https://docs.simplicite.io/">https://docs.simplicite.io/</a></p>
+          <p>Notre documentation : <a target="_blank" href="https://docs.simplicite.io/">https://docs.simplicite.io/</a>
+          </p>
           <p>Ici, vous trouverez la documentation de Simplicité.</p>
         </li>
 
       </ul>
-      <h4 class="end-text">Voilà, c'est tout ! Si vous êtes arrivés jusqu'ici, allez vérifier vos mails, vous devriez avoir une bonne surprise. A tout de suite sur Simplicité !</h4>
+      <h4 class="end-text">Voilà, c'est tout ! Si vous êtes arrivés jusqu'ici, allez vérifier vos mails, vous devriez
+        avoir une bonne surprise. A tout de suite sur Simplicité !</h4>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: 'Deployment',
+  export default {
+    name: 'Deployment',
 
-        mounted() {
-            let demandId = this.$router.currentRoute.path.split("/sandbox/")[1];
-            let baseURL = "https://portalpr.dev.simplicite.io/ext/PorIsdService?identifier=";
-            let req = new XMLHttpRequest();
-            req.open("GET", baseURL + demandId, true);
-            req.send(null);
-            console.log(req.responseText);
-        }
+    mounted() {
+      let demandId = this.$router.currentRoute.path.split("/sandbox/")[1];
+      let baseURL = "https://portalpr.dev.simplicite.io/ext/PorIsdService?identifier=";
+      let req = new XMLHttpRequest();
+      req.open("GET", baseURL + demandId, true);
+      req.send(null);
+      console.log(req.responseText);
     }
+  }
 
 </script>
 
@@ -96,7 +102,7 @@
 
   }
 
-  .sandbox-image{
+  .sandbox-image {
     max-width: $redirect-image-width;
     align-self: center;
     margin-bottom: map-get($margins, medium);
