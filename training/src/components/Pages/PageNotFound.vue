be9176daf3c9015735286e692b23217af3025138
maximebelgaid <max.belgaid@gmail.com>
2020-05-20 11:28:44 +0200

    Turned PageNotFound.vue to sass & simplified code

--- training/src/components/Pages/PageNotFound.vue
+++ training/src/components/Pages/PageNotFound.vue
@@ -1,34 +1,23 @@
 <template>
-  <div id="wrapper">
-    <h1 class="mx-auto">404 NOT FOUND, sorry, this page doesn't exist</h1>
+  <div class="not-found">
+    <h1>404 NOT FOUND, sorry, this page doesn't exist</h1>
   </div>
-
 </template>
 
 <script>
   export default {
     name: 'PageNotFound',
-    created() {
-      //TODO : Unset several parameters : currentLesson, currentLessonImages etc ...
-    }
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
+<style lang="sass" scoped>
+h1
+  color: black
 
-  @import "../../assets/sass/utils/variables";
-
-  h1 {
-    color: black;
-    // font-size: $h1-size;
-  }
-
-  #wrapper {
-    height: 80vh;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.not-found
+  height: 80vh
+  width: 100%
+  display: flex
+  justify-content: center
+  align-items: center
 </style>
