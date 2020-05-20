edd68863b34aedf6ba9e536be0ef0c35d8339214
maximebelgaid <max.belgaid@gmail.com>
2020-05-20 10:45:24 +0200

    Turned Spinner.vue to sass

--- training/src/components/UI/Spinner.vue
+++ training/src/components/UI/Spinner.vue
@@ -9,38 +9,33 @@
   };
 </script>
 
-<style lang="scss" scoped>
-  @keyframes spinner {
-    0% {
-      transform: translate3d(-50%, -50%, 0) rotate(0deg);
-    }
-    100% {
-      transform: translate3d(-50%, -50%, 0) rotate(360deg);
-    }
-  }
+<style lang="sass" scoped>
+@keyframes spinner
+  0%
+    transform: translate3d(-50%, -50%, 0) rotate(0deg)
 
-  .spinner {
-    // The height here is just for demo purposes
-    height: 100%;
-    opacity: 1;
-    position: relative;
-    transition: opacity linear 0.1s;
+  100%
+    transform: translate3d(-50%, -50%, 0) rotate(360deg)
 
-    &::before {
-      animation: 2s linear infinite spinner;
-      border: solid 3px #eee;
-      border-bottom-color: #EF6565;
-      border-radius: 50%;
-      content: "";
-      height: 100px;
-      left: 50%;
-      opacity: inherit;
-      position: absolute;
-      top: 50%;
-      transform: translate3d(-50%, -50%, 0);
-      transform-origin: center;
-      width: 100px;
-      will-change: transform;
-    }
-  }
+.spinner
+  // The height here is just for demo purposes
+  height: 100%
+  opacity: 1
+  position: relative
+  transition: opacity linear 0.1s
+  &::before
+    animation: 2s linear infinite spinner
+    border: solid 3px #eee
+    border-bottom-color: #EF6565
+    border-radius: 50%
+    content: ""
+    height: 100px
+    left: 50%
+    opacity: inherit
+    position: absolute
+    top: 50%
+    transform: translate3d(-50%, -50%, 0)
+    transform-origin: center
+    width: 100px
+    will-change: transform
 </style>
