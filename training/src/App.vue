0ff70baf6da9a9bbb47a97e22fac2b833929b4f0
maximebelgaid <max.belgaid@gmail.com>
2020-05-20 10:43:07 +0200

    created .sass stylesheets & defined props in a better way in TreeViewNode & turned TreeViewNode + App.vue in sass

--- training/src/App.vue
+++ training/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
-    <Header/>
 
+    <Header/>
     <main>
       <nav class="navigation-drawer" :class="[isDrawerOpen ? 'open' : '']">
         <TreeViewNode v-for="motherCategory in tree" :key="motherCategory.trnCatPath" :node="motherCategory" :depth="0"/>
@@ -10,11 +10,11 @@
         <router-view class="page-content__router-view" :key="$route.fullPath" v-if="tree.length > 1"/>
       </div>
     </main>
-
     <div class="popup" :class="[isPopupVisible ? 'visible' : '']">
       <div class="popup__overlay" @click="disablePopUpImage"></div>
       <img class="popup__image" :src="currentPopupImageSrc" alt="popup image"/>
     </div>
+
   </div>
 </template>
 
@@ -44,85 +44,67 @@
   };
 </script>
 
-<style lang="scss">
-  @import "assets/sass/utils/variables";
-  @import "assets/sass/utils/mixins";
+<style lang="sass">
+@import "assets/sass/utils/variables"
+@import "assets/sass/utils/mixins"
 
-  * {
-    font-family: 'Source Sans Pro', sans-serif;
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    outline: 0;
-  }
+*
+  font-family: 'Source Sans Pro', sans-serif
+  box-sizing: border-box
+  margin: 0
+  padding: 0
+  outline: 0
 
-  .app {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
+.app
+  height: 100%
+  display: flex
+  flex-direction: column
+  main
+    flex: 1 1 // So the main content extends to the bottom of the page
+    display: flex
+    flex-direction: row
+    width: 100%
+    position: relative
+    .navigation-drawer
+      display: block
+      width: 0
+      height: 100%
+      color: white
+      background: linear-gradient($color-primary 40%, $color-secondary)
+      transition: $duration-drawer-collapse ease-in-out
+      &.open
+        width: $drawer-width
 
-    main {
-      flex: 1 1; // So the main content extends to the bottom of the page
-      display: flex;
-      flex-direction: row;
-      width: 100%;
-      position: relative;
+    .page-content
+      width: 100%
+      &__router-view
+        width: 100%
+        height: 100%
+        padding: map-get($paddings, medium)
 
-      .navigation-drawer {
-        display: block;
-        width: 0;
-        height: 100%;
-        color: white;
-        background: linear-gradient($color-primary 40%, $color-secondary);
-        transition: $duration-drawer-collapse ease-in-out;
+.popup
+  position: absolute
+  z-index: -10000
+  visibility: hidden
+  width: 100%
+  height: 100%
+  &.visible
+    z-index: 1000
+    visibility: visible
 
-        &.open {
-          width: $drawer-width;
-        }
-      }
+  &__overlay
+    width: 100%
+    height: 100%
+    background-color: rgba(0, 0, 0, 0.4)
+    &:hover
+      cursor: pointer
 
-      .page-content {
-        width: 100%;
-
-        &__router-view {
-          width: 100%;
-          height: 100%;
-          padding: map-get($paddings, medium);
-        }
-      }
-    }
-  }
-
-  .popup {
-    position: absolute;
-    z-index: -10000;
-    visibility: hidden;
-    width: 100%;
-    height: 100%;
-
-    &.visible {
-      z-index: 1000;
-      visibility: visible;
-    }
-
-    &__overlay {
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.4);
-
-      &:hover {
-        cursor: pointer;
-      }
-    }
-
-    &__image {
-      border-radius: map-get($radius, regular);
-      max-width: 80%;
-      max-height: 80%;
-      position: absolute;
-      top: 10%;
-      left: 15%;
-    }
-  }
+  &__image
+    border-radius: map-get($radius, regular)
+    max-width: 80%
+    max-height: 80%
+    position: absolute
+    top: 10%
+    left: 15%
 </style>
 
--- training/src/components/UI/TreeViewNode.vue
+++ training/src/components/UI/TreeViewNode.vue
@@ -6,7 +6,8 @@
                 {{node.trnCatTitle}}
             </span>
       <span v-else
-            v-bind:class="[this.currentLesson.trnLsnPath === node.trnLsnPath ? 'active' : '', 'node__label']"
+            class="node__label"
+            v-bind:class="[this.currentLesson.trnLsnPath === node.trnLsnPath ? 'active' : '']"
             @click="handleClick(node)">
                 <i class="material-icons">menu_book</i>
                 {{node.trnLsnTitle}}
@@ -14,88 +15,85 @@
     </div>
 
     <div v-if="showChildren">
-      <TreeViewNode v-for="subCat in node.categories" v-bind:key="subCat.trnCatPath" :node="subCat"
+      <TreeViewNode v-for="subCategory in node.categories" v-bind:key="subCategory.trnCatPath" :node="subCategory"
                     :depth="depth+1"/>
-      <TreeViewNode v-for="lesson in node.lessons" v-bind:key="lesson.trnLsnPath" :node="lesson"
-                    :depth="depth+1"/>
+      <TreeViewNode v-for="lesson in node.lessons" v-bind:key="lesson.trnLsnPath" :node="lesson" :depth="depth+1"/>
     </div>
   </div>
 
 </template>
 
 <script>
-  import {mapState} from 'vuex';
+import {mapState} from 'vuex';
 
-  export default {
-    props: ['node', 'depth'],
-    name: "TreeViewNode",
-    data: () => ({
-      showChildren: true,
-    }),
-    computed: {
-      indent() {
-        return {transform: `translate(${this.depth * 20}px)`}
-      },
-      ...mapState(['currentLesson']),
+export default {
+  props: {
+    node: {
+      type: Object,
+      required: true,
+      default: () => {},
+      note: 'The node object containing the node\'s information and children nodes',
     },
-    methods: {
-      handleClick(node) {
-        if (node.trnCatPath)
-          this.showChildren = !this.showChildren;
-        else
-          this.$router.push('/lesson' + node.trnLsnPath).catch(err => console.error(err))
-      },
-    }
+    depth: {
+      type: Number,
+      required: true,
+      default: 1,
+      note: 'The depth of the node, to create the space before the node',
+    },
+  },
+  name: "TreeViewNode",
+  data: () => ({
+    showChildren: true,
+  }),
+  computed: {
+    ...mapState(['currentLesson']),
+    indent() {
+      return {transform: `translate(${this.depth * 20}px)`}
+    },
+  },
+  methods: {
+    handleClick(node) {
+      if (node.trnCatPath)
+        this.showChildren = !this.showChildren;
+      else
+        this.$router.push('/lesson' + node.trnLsnPath).catch(err => console.error(err))
+    },
   }
+}
 </script>
 
-<style lang="scss" scoped>
-  @import "../../assets/sass/utils/variables";
-  @import "../../assets/sass/utils/mixins";
+<style lang="sass" scoped>
+@import "../../assets/sass/variables"
+@import "../../assets/sass/mixins"
 
+.node-wrapper
+  //Really important for global rendering. Otherwise, when the side menu is collapsed, the height of the menu
+  //gets bigger than the page because the words in the treeview wrap
+  overflow: hidden
+  white-space: nowrap
 
-  .node-wrapper {
-    //Really important for global rendering. Otherwise, when the side menu is collapsed, the height of the menu
-    //gets bigger than the page because the words in the treeview wrap
-    overflow: hidden;
-    white-space: nowrap;
-  }
+.label-wrapper
+  overflow: hidden
+  .node__label
+    font-size: map-get($title-sizes, xx-small)
+    font-weight: lighter
+    display: flex
+    flex-direction: row
+    align-items: center
+    @include rounded-left-corners
+    padding: 8px
+    margin: 0
+    i
+      margin-right: map-get($margins, x-small)
 
-  .label-wrapper {
-    overflow: hidden;
+    &:hover
+      background-color: $color-tree-hover
+      cursor: pointer
 
-    .node__label {
-      font-size: map-get($title-sizes, xx-small);
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      @include rounded-left-corners;
-      padding: 8px;
-      margin: 0;
+    &.active
+      background-color: $color-tree-hover
+      color: white
 
-      i {
-        margin-right: map-get($margins, x-small);
-      }
-
-      &:hover {
-        background-color: $color-tree-hover;
-        cursor: pointer;
-      }
-
-      &.active {
-        background-color: $color-tree-hover;
-        color: white;
-
-        i {
-          color: #4D515C;
-        }
-
-      }
-
-      &.active:hover {
-        background-color: $color-tree-hover;
-      }
-    }
-  }
-
+    &.active:hover
+      background-color: $color-tree-hover
 </style>
