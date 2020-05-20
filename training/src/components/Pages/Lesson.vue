13c1965120f6ef543f15f1d10516018e8e5df859
maximebelgaid <max.belgaid@gmail.com>
2020-05-20 11:39:25 +0200

    Turned Lesson.vue to sass

--- training/src/components/Pages/Lesson.vue
+++ training/src/components/Pages/Lesson.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="lesson-item-wrapper">
+  <div class="lesson">
     <div class="grid-lesson">
       <div id="lesson-content" class="grid-item grid-item-content">
         <div v-if="this.currentLesson.row_id" class="occupy100percent">
@@ -128,198 +128,154 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-  @import "../../assets/sass/utils/variables";
-  @import "../../assets/sass/utils/mixins";
+<style lang="sass" scoped>
+@import "../../assets/sass/utils/variables"
+@import "../../assets/sass/utils/mixins"
+.lesson
+  position: relative
+
+.breadcrumb
+  border-bottom: $regular-thickness solid $light-grey
+  background-color: white
+  padding-bottom: 1em
+  &__item
+    text-transform: uppercase
+    &:hover
+      cursor: pointer
+  &__divider
+    margin: 0 $breadcrumb-margin 0 $breadcrumb-margin
+    text-transform: uppercase
+
+.grid-lesson
+  position: absolute
+  width: 100%
+  height: 100%
+  display: grid
+  grid-template-columns: repeat(2, 50%)
+  grid-template-rows: repeat(2, 50%)
+
+.grid-item
+  margin: 1em
+  //padding: 0.5em
+  background: white
+  border-radius: map-get($radius, regular)
+  @include box-shadow
+  overflow: auto
+
+.grid-item-content
+  grid-column: 1
+  grid-row: 1 / 3
+  padding: 1em
+
+.grid-item-media
+  grid-column: 2
+  grid-row: 1
+
+.grid-item-video
+  grid-column: 2
+  grid-row: 2
+
+.occupy100percent
+  height: 100%
+  width: 100%
+
+video
+  height: 100%
+  width: 100%
+
+/* ----- LESSON LESSON ----- */
+.lesson-title
+  color: $color-primary
+  font-size: map-get($title-sizes, x-large) + 1rem
+  font-weight: bold
+  margin-top: map-get($margins, medium)
+
+.lesson_content
+  @include flex-column-nowrap
+  overflow: hidden
+  /* ::v-deep is used instead of >>> because we are using sass (with scss syntax). it is a deep selector to apply styles to the v-html content*/
+  & ::v-deep h1
+    font-size: map-get($title-sizes, x-large)
+
+  & ::v-deep h2
+    font-size: map-get($title-sizes, large)
+    font-weight: bold
+    color: $color-secondary
+
+  & ::v-deep h3
+    font-size: map-get($title-sizes, medium)
+    font-weight: bold
+    color: $color-secondary
+
+  & ::v-deep h4
+    font-size: map-get($title-sizes, small)
+    color: $color-secondary
+
+  & ::v-deep h5
+    font-size: map-get($title-sizes, x-small)
+    color: $color-secondary
+
+  & ::v-deep h6
+    font-size: map-get($title-sizes, x-small) - 0.1rem
+    color: $color-secondary
+
+  & ::v-deep p
+    text-align: justify
+
+  & ::v-deep .info,
+  & ::v-deep .success,
+  & ::v-deep .warning,
+  & ::v-deep .error
+    border-radius: map-get($radius, regular)
+    padding: map-get($paddings, x-small)
+    margin: map-get($margins, x-small)
+
+  & ::v-deep .info
+    background-color: $color-information !important
+
+  & ::v-deep .success
+    background-color: $color-success !important
+
+  & ::v-deep .warning
+    background-color: $color-warning !important
+
+  & ::v-deep .error
+    background-color: $color-error !important
+
+  & ::v-deep blockquote > p::before
+    content: '" '
+
+  & ::v-deep blockquote > p::after
+    content: ' "'
+
+  & ::v-deep blockquote > p
+    font-style: italic
+
+  & ::v-deep strong
+    text-decoration: underline
+
+  & ::v-deep ol
+    list-style-type: decimal
+    padding-left: 25px //ol and ul require a certain amount of padding to display the style-type
+    //The use of the reset style in app is still to keep though, because it doesn't provoke other issues
+
+  & ::v-deep ul
+    list-style-type: disc
+    padding-left: 25px
+
+  & ::v-deep img
+    margin-left: $content-padding
 
 
-  .lesson-item-wrapper {
-    position: relative;
-  }
-
-  .grid-lesson {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    display: grid;
-    grid-template-columns: repeat(2, 50%);
-    grid-template-rows: repeat(2, 50%);
-  }
-
-  .grid-item {
-    margin: 1em;
-    //padding: 0.5em;
-    background: white;
-    border-radius: map-get($radius, regular);;
-    @include box-shadow;
-    overflow: auto;
-  }
-
-  .grid-item-content {
-    grid-column: 1;
-    grid-row: 1 / 3;
-    padding: 1em;
-  }
-
-  .grid-item-media {
-    grid-column: 2;
-    grid-row: 1;
-  }
-
-  .grid-item-video {
-    grid-column: 2;
-    grid-row: 2;
-  }
-
-  .occupy100percent {
-    height: 100%;
-    width: 100%;
-  }
-
-  video {
-    height: 100%;
-    width: 100%;
-  }
-
-  .breadcrumb {
-    border-bottom: $regular-thickness solid $light-grey;
-    background-color: white;
-    padding-bottom: 1em;
-
-    &__item {
-      text-transform: uppercase;
-
-      &:hover {
-        cursor: pointer;
-      }
-    }
-
-    &__divider {
-      margin: 0 $breadcrumb-margin 0 $breadcrumb-margin;
-      text-transform: uppercase;
-    }
-  }
-
-  /* ----- LESSON LESSON ----- */
-  .lesson-title {
-    color: $color-primary;
-    font-size: map-get($title-sizes, x-large) + 1rem;
-    font-weight: bold;
-    margin-top: map-get($margins, medium);
-  }
-
-  .lesson_content {
-    @include flex-column-nowrap;
-    overflow: hidden;
-
-
-    & ::v-deep h1 {
-      font-size: map-get($title-sizes, x-large);
-    }
-
-    & ::v-deep h2 {
-      /* ::v-deep is used instead of >>> because we are using sass (with scss syntax). it is a deep selector to apply styles to the v-html content*/
-      font-size: map-get($title-sizes, large);
-      font-weight: bold;
-      color: $color-secondary;
-    }
-
-    & ::v-deep h3 {
-      font-size: map-get($title-sizes, medium);
-      font-weight: bold;
-      color: $color-secondary;
-    }
-
-    & ::v-deep h4 {
-      font-size: map-get($title-sizes, small);
-      color: $color-secondary;
-    }
-
-    & ::v-deep h5 {
-      font-size: map-get($title-sizes, x-small);
-      color: $color-secondary;
-    }
-
-    & ::v-deep h6 {
-      font-size: map-get($title-sizes, x-small) - 0.1rem;
-      color: $color-secondary;
-    }
-
-    & ::v-deep p {
-      text-align: justify;
-    }
-
-    & ::v-deep .info,
-    & ::v-deep .success,
-    & ::v-deep .warning,
-    & ::v-deep .error {
-      border-radius: map-get($radius, regular);;
-      padding: map-get($paddings, x-small);
-      margin: map-get($margins, x-small);
-    }
-
-    & ::v-deep .info {
-      background-color: $color-information !important;
-    }
-
-    & ::v-deep .success {
-      background-color: $color-success !important;
-    }
-
-    & ::v-deep .warning {
-      background-color: $color-warning !important;
-    }
-
-    & ::v-deep .error {
-      background-color: $color-error !important;
-    }
-
-    & ::v-deep blockquote > p::before {
-      content: '" ';
-    }
-
-    & ::v-deep blockquote > p::after {
-      content: ' "';
-    }
-
-    & ::v-deep blockquote > p {
-      font-style: italic;
-    }
-
-    & ::v-deep strong {
-      text-decoration: underline;
-    }
-
-    & ::v-deep ol {
-      list-style-type: decimal;
-      padding-left: 25px; //ol and ul require a certain amount of padding to display the style-type
-      //The use of the reset style in app is still to keep though, because it doesn't provoke other issues
-    }
-
-    & ::v-deep ul {
-      list-style-type: disc;
-      padding-left: 25px;
-    }
-
-    & ::v-deep img {
-      margin-left: $content-padding;
-    }
-  }
-
-  .toggle-scroll {
-    border-radius: map-get($radius, x-large);
-    padding: map-get($paddings, medium);
-    background-color: white;
-    font-weight: bold;
-    color: green;
-    border: solid 2px $color-accent;
-    outline: none;
-
-    &.off {
-      border-color: red;
-      color: red;
-    }
-
-
-  }
+.toggle-scroll
+  border-radius: map-get($radius, x-large)
+  padding: map-get($paddings, medium)
+  background-color: white
+  font-weight: bold
+  color: green
+  border: solid 2px $color-accent
+  outline: none
+  &.off
+    border-color: red
+    color: red
 </style>
