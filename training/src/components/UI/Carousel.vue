8d71ea160b9f883d24e160e80e85010ac430d985
maximebelgaid <max.belgaid@gmail.com>
2020-05-20 11:12:54 +0200

    Turned Carousel.vue to sass and defined props the correct way

--- training/src/components/UI/Carousel.vue
+++ training/src/components/UI/Carousel.vue
@@ -1,16 +1,16 @@
 <!-- https://christianheilmann.com/2015/04/08/keeping-it-simple-coding-a-carousel/ -->
 <!-- https://github.com/codepo8/simple-carousel/blob/master/carousel-images.html -->
 <template>
-  <div class="carousel-wrapper">
-    <div class="img-holder">
+  <div class="carousel">
+    <div class="carousel__img-holder">
       <transition>
-        <img :src="currentImg.filesrc" :download="currentImg.filename" v-bind:key="currentImg.filename"
-             @click="displayFullScreenImage(currentImg.filesrc)">
+        <img :src="currentImg.filesrc" alt="carousel image" :download="currentImg.filename"
+             v-bind:key="currentImg.filename" @click="displayFullScreenImage(currentImg.filesrc)">
       </transition>
     </div>
-    <div class="buttons">
+    <div class="carousel__buttons">
       <button @click="navigate(-1)"> ◀</button>
-      <p class="img-counter">{{(this.counter + 1)}} / {{this.images.length}}</p>
+      <p class="carousel__img-counter">{{(this.counter + 1)}} / {{this.images.length}}</p>
       <button @click="navigate(1)"> ▶</button>
     </div>
   </div>
@@ -18,21 +18,26 @@
 </template>
 
 <script>
-  //import ExpandableImage from "../../UI/ExpandableImage";
   export default {
     name: "Carousel",
-    //components: {ExpandableImage},
-    props: ["images"],
+    props: {
+      images: {
+        type: Array,
+        required: true,
+        default: () => [],
+        note: 'The images in the carousel',
+      },
+    },
     data: () => ({
       counter: 0
     }),
     computed: {
-      currentImg: function () {
+      currentImg() {
         return this.images[this.counter];
       }
     },
     methods: {
-      navigate: function (direction) {
+      navigate(direction) {
         this.counter = (this.counter + direction) % this.images.length;
         this.counter = this.counter < 0 ? this.images.length - 1 : this.counter;
       },
@@ -52,7 +57,7 @@
       }
     },
     mounted() {
-      if (this.images.length <= 1) {
+      if (!this.images.length) {
         let buttons = document.querySelector(".buttons");
         buttons.style.visibility = "hidden";
       } else {
@@ -75,74 +80,54 @@
   };
 </script>
 
-<style lang="scss" scoped>
-  @import "../../assets/sass/utils/variables";
+<style lang="sass" scoped>
+@import "../../assets/sass/utils/variables"
+.carousel
+  width: 100%
+  height: 100%
+  position: relative
+  margin: 0
+  overflow: hidden // avoid transition scrollbar
 
-  img {
-    max-width: 100%;
-    max-height: 100%;
-  }
+  &__buttons
+    width: 100%
+    height: 40px
+    position: absolute
+    bottom: 0
+    padding: 5px 0
+    text-align: center
+    background: #eee
+    z-index: 10
+    visibility: hidden
+    display: flex
+    justify-content: center
+    align-items: center
+    button
+      border: none
 
-  .image {
-    max-width: 100%;
-    max-height: 100%;
-  }
-
-  .carousel-wrapper {
-    width: 100%;
-    height: 100%;
-    position: relative;
-    margin: 0;
-    overflow: hidden; // avoid transition scrollbar
-  }
-
-  .buttons {
-    width: 100%;
-    height: 40px;
-    position: absolute;
-    bottom: 0;
-    padding: 5px 0;
-    text-align: center;
-    background: #eee;
-    z-index: 10;
-    visibility: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .img-counter {
-    height: 100%;
+  &__img-counter
+    height: 100%
     margin: 0 $img-counter-margin 0 $img-counter-margin
-  }
+
+  &__img-holder
+    &:hover
+      cursor: pointer
+    img
+      max-width: 100%
+      max-height: 100%
 
   /* Transition */
-  .v-enter, .v-leave-to {
-    //transform: scale(0);
-    opacity: 0;
-  }
+  .v-enter, .v-leave-to
+    //transform: scale(0)
+    opacity: 0
 
-  .v-enter-to, .v-leave {
-    //transform: scale(1);
-    opacity: 1;
-  }
+  .v-enter-to, .v-leave
+    //transform: scale(1)
+    opacity: 1
 
-  .v-leave-active, .v-enter-active {
-    transition-duration: 0.2s;
-  }
+  .v-leave-active, .v-enter-active
+    transition-duration: 0.2s
 
-  .v-enter-active {
-    transition-delay: 0.2s;
-  }
-
-  button {
-    border: none;
-  }
-
-
-  .img-holder {
-    &:hover {
-      cursor: pointer;
-    }
-  }
+  .v-enter-active
+    transition-delay: 0.2s
 </style>
